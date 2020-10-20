b43340d6edbc6a17b30ce1f6305dd5eb77833b62
Vitalii <demedyuk.v.i@gmail.com>
2020-10-21 02:33:32 +0300

    Previous user interface.

--- MiniSpreadsheet.pro
+++ MiniSpreadsheet.pro
@@ -18,10 +18,10 @@ HEADERS += \
 FORMS += \
     mainwindow.ui
 
-TRANSLATIONS += \
-    MiniSpreadsheet_en_US.ts
-
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    images.qrc
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/img/images/splash_screen.png"));
+    splash->show();
     MainWindow w;
     w.show();
+    splash->finish(&w);
+    delete splash;
     return a.exec();
 }
